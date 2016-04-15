341a38f0ce75f27a07dff72c81a59104a7a1647d
duynhm <dzumihn@gmail.com>
2016-04-15 21:07:38 +0700

    commit demo

---
+++ app/src/main/java/dzumi/app/demo/demot3h/modules/resource/SupportMultiLangActivity.java
@@ -0,0 +1,15 @@
+package dzumi.app.demo.demot3h.modules.resource;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import dzumi.app.demo.demot3h.R;
+
+public class SupportMultiLangActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_support_multi_lang);
+    }
+}
